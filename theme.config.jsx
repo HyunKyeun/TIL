0b1126b1583ea2afda3b8687f50296d1b7a7ba26
HyunKyeun <dango11371137@gmail.com>
2023-07-04 09:46:44 +0900

    a

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,13 +2,16 @@
 export default {
   editLink: { text: "" },
   feedback: { content: "" },
-  logo: <strong>HyunKyeun</strong>,
+  logo: <strong>개발새발</strong>,
   useNextSeoProps() {
     return {
-      titleTemplate: "%s – Na",
+      titleTemplate: "%s – HyunKyeun.Na",
     };
   },
   sidebar: {
     toggleButton: true,
+  },
+  footer: {
+    text: <span>© 2023 HyunKyeun Na</span>,
   },
 };
